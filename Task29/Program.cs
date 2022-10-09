f477be33b17c090bea0104ffbfd4974f6d69a462
Sofia Kabanova <sofia.kabanova@gmail.com>
2022-10-09 21:15:48 +0300

    change 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,49 +3,52 @@
 // и выводит их на экран.
 //  Например:
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
-// Console.WriteLine("Введите  длинну массива:");
+Console.WriteLine("Введите  длинну массива:");
 //  если запрашиваем длинну массива у пользователя
-// int length = Convert.ToInt32(Console.ReadLine());
-int length = 8; // вводим длинну массива = 8 элементов
+int length = Convert.ToInt32(Console.ReadLine());
+// int length = 8; // вводим длинну массива = 8 элементов
+Console.WriteLine("Введите диапазон чисел массива:");
+Console.Write("Min: ");
+int minNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Max: ");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
 int[] arr = new int [length]; //создали массив из length элементов
 // int по умолчанию инициализируется нулем, поэтому данный массив
 // состоит из восьми нулей: [0,0,0,0,0,0,0,0]
 
-void FillArray(int[] array)// делаем метод, который заполняет массив
+void FillArray(int[] array, int min, int max)// делаем метод, который заполняет массив
 {
     Random rnd = new Random();
     for (int i = 0; i<length; i++)
     {
         // arr[i] = new Random().Next(0, 10); 
         //записываем в индекс i случайное значение от 0 до 10
-        arr[i] = rnd.Next(); 
+        arr[i] = rnd.Next(min,max); 
         //записываем в индекс i случайное число
     }
 }
 void PrintNumbers(int[] array) 
 {
-    for (int i = 0; i+1 < length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{arr[i]}, ");
-    }   
+       if(i<array.Length - 1) Console.Write($"{array[i]}, ");
+       else Console.Write($"{array[i]} ");
+    }
 }
-void PrintNumbersLast(int[] array) 
-{
-        Console.Write(arr[^1]);
-}
+
 void PrintArray(int[] array) 
 {
-    for (int i = 0; i+1 < length; i++)
+     Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{arr[i]}, ");
-    }   
+       if(i<array.Length - 1) Console.Write($"{array[i]}, ");
+       else Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
 }
 Console.WriteLine($"Массив из {length} чисел: ");
-FillArray(arr);
+FillArray(arr, minNumber, maxNumber);
 PrintNumbers(arr);
-PrintNumbersLast(arr);
-Console.Write($" - > [ ");
+Console.Write($" - > ");
 PrintArray(arr);
-PrintNumbersLast(arr);
-Console.Write($" ]");
 
